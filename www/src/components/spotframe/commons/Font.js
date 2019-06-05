92faa1ecfc4184a3909ddb4acc72bf1cdb5f04b8
Naner Nunes <naner@naner.com.br>
2019-06-05 00:19:05 -0300

    Using props.Text instead of invalid props.children

--- www/src/components/spotframe/commons/Font.js
+++ www/src/components/spotframe/commons/Font.js
@@ -5,15 +5,19 @@ class Font extends Component {
   render() {
     return (
       <font
-        face={this.props.face}
-        size={this.props.size}
-        color={this.props.color}
+        face={this.props.Face}
+        size={this.props.Size}
+        color={this.props.Color}
       >
-        {this.props.children}
+        {this.props.Text}
       </font>
     )
   }
 
+}
+
+Font.propTypes = {
+  Text: PropTypes.string.isRequired,
 }
 
 export default Font
